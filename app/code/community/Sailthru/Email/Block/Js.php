165243aa1c155250aa55bf20dfcfc156567363d8
Alex Silverman <asilverman@sailthru.com>
2017-02-27 20:20:39 -0500

    renamed block from horizon to js, and removed log lines

--- app/code/community/Sailthru/Email/Block/Js.php
+++ app/code/community/Sailthru/Email/Block/Js.php
@@ -8,19 +8,16 @@
  */
 
 
-class Sailthru_Email_Block_Horizon extends Mage_Page_Block_Html_Head
+class Sailthru_Email_Block_Js extends Mage_Page_Block_Html_Head
 {
-    private $sailthruHelper;
-
     protected function _construct() {
         $sailthruHelper = Mage::helper('sailthruemail');
         if ($sailthruHelper->isPersonalizeJsEnabled() and $sailthruHelper->getCustomerId()) {
-            Mage::log('loading SPM!', null, 'sailthru.log');
             $this->setTemplate("sailthru/spm.phtml");
         } elseif ($sailthruHelper->isHorizonEnabled() and $sailthruHelper->getHorizonDomain()) {
-            Mage::log("loading Horizon!", null, "sailthru.log");
             $this->setTemplate("sailthru/horizon.phtml");
         }
+        parent::_construct();
     }
 
 }
