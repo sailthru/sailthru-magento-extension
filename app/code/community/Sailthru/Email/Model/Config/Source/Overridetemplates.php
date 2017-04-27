e77c55d5952ed3fc3c7fdd46de3a3580d9509211
Alex Silverman <asilverman@sailthru.com>
2017-04-27 11:11:48 -0400

    Fixed error handling for System Configuration

--- app/code/community/Sailthru/Email/Model/Config/Source/Overridetemplates.php
+++ app/code/community/Sailthru/Email/Model/Config/Source/Overridetemplates.php
@@ -1,6 +1,6 @@
 <?php
 
-class Sailthru_Email_Model_Config_Source_Overridetemplates extends Sailthru_Email_Model_Client {
+class Sailthru_Email_Model_Config_Source_Overridetemplates extends Sailthru_Email_Model_Config_Source {
 
     /**
      * Options getter
@@ -10,20 +10,21 @@ class Sailthru_Email_Model_Config_Source_Overridetemplates extends Sailthru_Emai
     public function toOptionArray()
     {
         $this->_eventType = "MagentoSettings";
-        $response = $this->apiGet("template");
-        if (isset($response["error"])){
-            return [['value'=>0, 'label'=>__('Please Enter Valid API Credentials')]];
-        }
-        $templates = $response["templates"];
-        $tpl_options = [
-            ['value'=> 0, 'label'=>'Use Magento template']
-        ];
-        foreach ($templates as $tpl) {
-            $tpl_options[] = [
-                'value' => $tpl['name'],
-                'label' => __($tpl['name'])
+        try {
+            $response = $this->apiGet("template");
+            $templates = $response["templates"];
+            $tpl_options = [
+                ['value'=> 0, 'label'=>'Use Magento template']
             ];
+            foreach ($templates as $tpl) {
+                $tpl_options[] = [
+                    'value' => $tpl['name'],
+                    'label' => __($tpl['name'])
+                ];
+            }
+            return $tpl_options;
+        } catch (Sailthru_Client_Exception $e) {
+            return $this->processOptionArrayError($e);
         }
-        return $tpl_options;
     }
 }
--- app/code/community/Sailthru/Email/Model/Config/Source/Sailthrutemplates.php
+++ app/code/community/Sailthru/Email/Model/Config/Source/Sailthrutemplates.php
@@ -4,7 +4,7 @@
  * Used in creating options for List Selection from Sailthru, or inactive if API keys are invalid.
  *
  */
-class Sailthru_Email_Model_Config_Source_Sailthrutemplates extends Sailthru_Email_Model_Client
+class Sailthru_Email_Model_Config_Source_Sailthrutemplates extends Sailthru_Email_Model_Config_Source
 {
     /**
      * Options getter
@@ -13,21 +13,25 @@ class Sailthru_Email_Model_Config_Source_Sailthrutemplates extends Sailthru_Emai
      */
     public function toOptionArray()
     {
-        $this->_eventType = "MagentoSettings";
-        $response = $this->apiGet("template");
-        if (isset($response["error"])){
-            return [['value'=>0, 'label'=>__('Please Enter Valid API Credentials')]];
-        }
-        $templates = $response["templates"];
-        $tpl_options = [
-            ['value'=> 0, 'label'=>'']
-        ];
-        foreach ($templates as $tpl) {
+        try {
+            $this->_eventType = "MagentoSettings";
+            $response = $this->apiGet("template");
+            if (isset($response["error"])) {
+                return [ [ 'value' => 0, 'label' => __('Please Enter Valid API Credentials') ] ];
+            }
+            $templates = $response["templates"];
+            $tpl_options = [
+                [ 'value' => 0, 'label' => '' ]
+            ];
+            foreach ($templates as $tpl) {
                 $tpl_options[] = [
                     'value' => $tpl['name'],
                     'label' => __($tpl['name'])
                 ];
+            }
+            return $tpl_options;
+        } catch (Sailthru_Client_Exception $e) {
+            $this->processOptionArrayError($e);
         }
-        return $tpl_options;
     }
 }
